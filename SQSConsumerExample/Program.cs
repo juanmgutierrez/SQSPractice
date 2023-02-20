eca584df2e976822902acf58f71366ace34feeb2
juanmgutierrez <jmgutierrez@invertironline.com>
2023-02-20 20:10:48 -0300

    Added SystemAttributes to console Consumer

--- SQSConsumerExample/Program.cs
+++ SQSConsumerExample/Program.cs
@@ -10,7 +10,9 @@ var sqsQueueUrlResponse = await sqsClient.GetQueueUrlAsync(queueName);
 var usersReceiveMessageRequest = new ReceiveMessageRequest()
 {
     QueueUrl = sqsQueueUrlResponse.QueueUrl,
-    MessageAttributeNames = new List<string>() { "ALL" }
+    MessageAttributeNames = new() { "All" },
+    AttributeNames = new() { "SenderId", "ApproximateReceiveCount", "SentTimestamp", "ApproximateFirstReceiveTimestamp" },
+    MaxNumberOfMessages = 2
 };
 
 while (!cancellationTokenSource.IsCancellationRequested)
@@ -20,13 +22,17 @@ while (!cancellationTokenSource.IsCancellationRequested)
 
     foreach (var message in receivedMessage.Messages)
     {
-        var messageId = message.MessageId;
-        Console.WriteLine(messageId);
-        Console.WriteLine(message.Body);
+        Console.WriteLine($"Message ID: {message.MessageId}");
+        Console.WriteLine($"Message Body: {message.Body}");
+        Console.WriteLine($"Message Type: {(message.MessageAttributes.TryGetValue("MessageType", out var messageType) ? messageType.StringValue : "Unknown")}");
+        Console.WriteLine($"\tSender ID: {(message.Attributes.TryGetValue("SenderId", out var senderId) ? senderId : "Unknown")}");
+        Console.WriteLine($"\tReceive Count: {(message.Attributes.TryGetValue("ApproximateReceiveCount", out var receiveCount) ? receiveCount : "Unknown")}");
+        Console.WriteLine($"\tSent Timestamp: {(message.Attributes.TryGetValue("SentTimestamp", out var sentTimestamp) ? sentTimestamp : "Unknown")}");
+        Console.WriteLine($"\tFirst Receive Timestamp: {(message.Attributes.TryGetValue("ApproximateFirstReceiveTimestamp", out var firstReceiveTimestamp) ? firstReceiveTimestamp : "Unknown")}");
         Console.WriteLine();
 
         var deletedMessageResponse = await sqsClient.DeleteMessageAsync(sqsQueueUrlResponse.QueueUrl, message.ReceiptHandle, cancellationTokenSource.Token);
-        Console.WriteLine($"Deleted MessageId = {messageId} with Status Code = {deletedMessageResponse.HttpStatusCode}");
+        Console.WriteLine($"Deleted MessageId = {message.MessageId} with Status Code = {deletedMessageResponse.HttpStatusCode}");
     }
 
     await Task.Delay(3000);
